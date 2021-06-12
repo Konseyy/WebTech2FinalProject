56e5fefb4c65b25bb4b906e2eb007045615e0ff9
Konsey <vgbukalders@gmail.com>
2021-06-12 10:08:52 +0300

    login page changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,12 +10,17 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <div class="col-md-6 offset-md-3">
+                                <a class="btn btn-success btn-block">Login with Google</a>
+                                <a class="btn btn-primary btn-block">Login with Facebook</a>
+                            </div>
+                        </div>
+                        <p style="text-align: center">or</p>
+                        <div class="form-group row">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-6 offset-md-3">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +31,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <div class="col-md-6 offset-md-3">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
